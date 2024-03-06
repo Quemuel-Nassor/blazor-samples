0fe9801f3108b0102f6dc060eb4d311fc7b15fe9
Quemuel Alves Nassor <quemuelalp@hotmail.com>
2024-03-06 20:30:16 -0300

    updated layout and functionalities

--- blazor-samples/Components/Wizard/StepItem.cs
+++ blazor-samples/Components/Wizard/StepItem.cs
@@ -2,15 +2,68 @@ namespace blazor_samples.Components.Wizard;
 
 public class StepItem
 {
+    public Guid Id { get; }
     public string Name { get; set; }
-    public string Slug => Name.Replace(" ", "-");
+    public int Index { get; set; }
+    public string Slug => Name?.Replace(" ", "-").ToLower();
     public string Icon { get; set; }
     public bool Active { get; set; }
-    public bool Enabled { get; set; } = true;
-    public bool Completed { get; set; }
-    
+    public bool Enabled { get; set; }
+    public bool Completed { get; private set; }
+
     public string GetClass()
     {
         return $" {(Active ? "active" : "")} {(Completed ? "completed" : "")} ";
     }
+
+    public StepItem()
+    {
+        Id = Guid.NewGuid();
+        PublisherHandler.Publisher.StepItemChangedEvent += ChangedEventHandler;
+        PublisherHandler.Publisher.EnableAllStepsEvent += EnableStepEventHandler;
+    }
+
+    void ChangedEventHandler(Object sender, StepItem e)
+    {
+        if (e.Id != Id)
+            this.Active = false;
+
+        if (e.Index > Index)
+            this.Completed = true;
+    }
+
+    void EnableStepEventHandler(Object sender, bool e)
+    {
+        Enabled = true;
+    }
+
+    ~StepItem(){
+        PublisherHandler.Publisher.StepItemChangedEvent -= ChangedEventHandler;
+        PublisherHandler.Publisher.EnableAllStepsEvent -= EnableStepEventHandler;
+    }
+}
+
+public class StepItemEventPublisher
+{
+    public event EventHandler<StepItem> StepItemChangedEvent;
+    public event EventHandler<bool> EnableAllStepsEvent;
+
+    public void EnableAllSteps()
+    {
+        EnableAllStepsEvent(this, true);
+    }
+
+    public void OnChanged(StepItem changedItem)
+    {
+        StepItemChangedEvent(this, changedItem);
+    }
+}
+
+public static class PublisherHandler
+{
+    public static readonly StepItemEventPublisher Publisher = new();
+
+    public static void OnChanged(StepItem e) => Publisher.OnChanged(e);
+
+    public static void EnableAllSteps() => Publisher.EnableAllSteps();
 }
--- blazor-samples/Components/Wizard/WizardComponent.razor.cs
+++ blazor-samples/Components/Wizard/WizardComponent.razor.cs
@@ -3,26 +3,57 @@ namespace blazor_samples.Components.Wizard;
 
 public partial class WizardComponent : ComponentBase
 {
+    StepItem ActiveStep { get; set; }
+
     [Parameter]
     public List<StepItem> Steps { get; set; } = new();
 
+    public void Next()
+    {
+        if (ActiveStep != null)
+        {
+            if (ActiveStep.Index < Steps.Count - 1)
+            {
+                ActiveStep = Steps[ActiveStep.Index + 1];
+                ActiveStep.Active = true;
+                ChangeStep(ActiveStep);
+            }
+        }
+    }
+
+    public void Previous()
+    {
+        if (ActiveStep != null)
+        {
+            if (ActiveStep.Index > 0)
+            {
+                ActiveStep = Steps[ActiveStep.Index - 1];
+                ActiveStep.Active = true;
+                ChangeStep(ActiveStep);
+            }
+        }
+    }
+
     void ChangeStep(StepItem step)
     {
-        int i = Steps.IndexOf(step);
-
-        if (i > 0)
-            Steps[i - 1].Completed = true;
-
-        for (int j = 0; j < Steps.Count; j++)
-            Steps[j].Active = false;
-
         step.Active = true;
         step.Enabled = true;
-    }    
+        ActiveStep = step;
+        
+        PublisherHandler.OnChanged(step);
+    }
 
-    protected override void OnInitialized()
+    protected override void OnAfterRender(bool firstRender)
     {
-        var step = Steps.FirstOrDefault();
-        if(step != null) step.Active = true;
+        if (firstRender)
+        {
+            ActiveStep = Steps.FirstOrDefault();
+            if (ActiveStep != null)
+            {
+                ActiveStep.Active = true;
+                ActiveStep.Enabled = true;
+                PublisherHandler.OnChanged(ActiveStep);
+            }
+        }
     }
 }
