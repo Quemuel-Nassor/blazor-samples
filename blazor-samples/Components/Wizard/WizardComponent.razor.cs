fe551f0d01f09d9ee560b86da502962c9445d5c5
Quemuel Alves Nassor <quemuelalp@hotmail.com>
2024-03-06 21:57:26 -0300

    enabled binding and centralized functionalities

--- blazor-samples/Components/Wizard/WizardComponent.razor.cs
+++ blazor-samples/Components/Wizard/WizardComponent.razor.cs
@@ -8,52 +8,50 @@ public partial class WizardComponent : ComponentBase
     [Parameter]
     public List<StepItem> Steps { get; set; } = new();
 
-    public void Next()
+    [Parameter]
+    public EventCallback<List<StepItem>> StepsChanged { get; set; }
+
+    public async Task EnableAllSteps()
+    {
+        PublisherHandler.EnableAllSteps();
+        await StepsChanged.InvokeAsync(Steps);
+    }
+
+    public async Task Next()
     {
         if (ActiveStep != null)
         {
             if (ActiveStep.Index < Steps.Count - 1)
-            {
-                ActiveStep = Steps[ActiveStep.Index + 1];
-                ActiveStep.Active = true;
-                ChangeStep(ActiveStep);
-            }
+                await ChangeStep( Steps[ActiveStep.Index + 1]);
         }
     }
 
-    public void Previous()
+    public async Task Previous()
     {
         if (ActiveStep != null)
         {
             if (ActiveStep.Index > 0)
-            {
-                ActiveStep = Steps[ActiveStep.Index - 1];
-                ActiveStep.Active = true;
-                ChangeStep(ActiveStep);
-            }
+                await ChangeStep(Steps[ActiveStep.Index - 1]);
         }
     }
 
-    void ChangeStep(StepItem step)
+    async Task ChangeStep(StepItem step)
     {
         step.Active = true;
         step.Enabled = true;
         ActiveStep = step;
         
         PublisherHandler.OnChanged(step);
+        await StepsChanged.InvokeAsync(Steps);
     }
 
-    protected override void OnAfterRender(bool firstRender)
+    protected override async void OnAfterRender(bool firstRender)
     {
         if (firstRender)
         {
             ActiveStep = Steps.FirstOrDefault();
             if (ActiveStep != null)
-            {
-                ActiveStep.Active = true;
-                ActiveStep.Enabled = true;
-                PublisherHandler.OnChanged(ActiveStep);
-            }
+                await ChangeStep(ActiveStep);
         }
     }
 }
